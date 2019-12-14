e12e1d62b27663ee24e22bc7239ecb6d0fd15cd1
sheerluck <sheer.luck.andrew@gmail.com>
2019-12-14 12:46:43 +0300

    :recycle: move options to nim.cfg

--- mkv.nim
+++ mkv.nim
@@ -1,14 +1,5 @@
 import os, system, strutils, argparse
 
-{.passC: """-std=c++2a -pipe -march=native -mtune=native -O3 \
-            -maes -mavx -mavx2 -mf16c -mfma -mmmx -mpclmul   \
-            -mpopcnt -msse -msse2 -msse3 -msse4.1 -msse4.2   \
-            -mssse3 -pedantic -Wextra -Wshadow -fipa-pta     \
-            -Wnon-virtual-dtor -Wdelete-non-virtual-dtor     \
-            -fgraphite-identity -fno-semantic-interposition  \
-            -floop-nest-optimize -fdevirtualize-at-ltrans    \
-        -fuse-linker-plugin -falign-functions=32 -flto=9""" .}
-
 var p = new_parser("mkv"):
   help("Longest movies (in current directory)")
   option("-p", "--path", default=".", help="Path to movies.")
@@ -16,7 +7,6 @@ var p = new_parser("mkv"):
   flag(  "-f", "--flat", help="No recursion.")
 
 var opts = p.parse(command_line_params())
-
 if opts.path == ".":
   opts.path = get_current_dir()
   
