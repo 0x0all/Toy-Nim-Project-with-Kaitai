3cf2ea11f94511b56470d84f15e1cba9b26a44bc
sheerluck <sheer.luck.andrew@gmail.com>
2019-12-16 11:01:36 +0300

    :construction: magic

--- ksy_helper.nim
+++ ksy_helper.nim
@@ -1,3 +1,4 @@
+import os, system, sugar, strutils, strformat
 import kaitai_struct_nim_runtime
 import ksy/mkv
 import ksy/mp4
@@ -5,8 +6,25 @@ import ksy/mp4_atom
 
 
 proc mkv_duration*(ks: KaitaiStream): float =
-  #var o = mkv_t(pks);
-  a(2020).to_float
+  var
+    o = new_mkv_t(ks)
+    offset = 0'u64
+    a, b, c = 0'u8
+
+  while true:
+    echo "sleep"
+    sleep(1000)
+
+
+  o.read()
+  echo o.magic
+  echo o.protocol
+  echo o.value4
+  echo o.value8
+  case cast[Size_type_t]  (o.protocol)
+  of Size_type_t.st_float: o.value4
+  else:                    o.value8
+
 
 
 proc mp4_duration*(ks: KaitaiStream, maybe_mpeg2: bool): float = 3.14
